026030ec0ed47942ede0317980afb244f149b95c
johnSickels <46609057+johnsickels@users.noreply.github.com>
2019-07-01 17:15:00 -0400

    Article model --> unique:true --> no repeating articles saved to db

--- models/Article.js
+++ models/Article.js
@@ -5,7 +5,8 @@ var Schema = mongoose.Schema;
 var ArticleSchema = new Schema({
     title: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     link: String,
     image: String,
